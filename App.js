05f9a63c7ecb5c851154a9ea51361744a6a9706f
Steven Huang <steven.charles.huang@gmail.com>
2018-08-25 00:08:14 -0400

    added text, created test component, imported component

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import HelloComponent from './components/HelloComponent';
 
 export default class App extends React.Component {
   render() {
@@ -8,6 +9,8 @@ export default class App extends React.Component {
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+        <Text>Hello World</Text>
+        <HelloComponent />
       </View>
     );
   }
---
+++ components/HelloComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class HelloComponent extends React.Component {
+  render() {
+    return(
+      <View>
+        <Text>Hello from Hello Component</Text>
+      </View>
+    );
+  }
+}
